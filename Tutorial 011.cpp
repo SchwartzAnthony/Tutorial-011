c769c229234bdac6bed35f14c96dc9bb6dae6a78
SchwartzAJ <schwartzanthonyj@gmail.com>
2023-02-01 09:36:04 +0100

    Add project files.

---
+++ Tutorial 011.cpp
@@ -0,0 +1,18 @@
+// Tutorial 011.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "add.h"
+
+// Header files 
+
+int add(int x, int y); 
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; 
+
+    return 0; 
+}
+
+
